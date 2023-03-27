ae003310229527f3602c0adaa41668f6370f5870
asad406 <asadl76@gmail.com>
2023-03-27 21:26:49 +0600

    init ema-jhon-shop-1

---
+++ src/components/Product/Product.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCartShopping } from '@fortawesome/free-solid-svg-icons';
+import './Product.css';
+
+const Product = (props) => {
+  const { id, name, price, ratings, seller, img, quantity } = props.product;
+  const handleAddToCart = props.handleAddToCart;
+  return (
+    <div className="product">
+      <img
+        src={img}
+        alt=""
+      />
+      <div className="product-info">
+        <h6 className="product-name">{name}</h6>
+        <p>Price: {price}</p>
+        <p>Manufacturer: {seller}</p>
+        <p>Rating: {ratings}</p>
+      </div>
+      <button
+        onClick={() => handleAddToCart(props.product)}
+        className="btn-cart"
+      >
+        Add to Cart  
+        <FontAwesomeIcon icon={faCartShopping} />
+      </button>
+    </div>
+  );
+};
+
+export default Product;
